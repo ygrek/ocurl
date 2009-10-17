f84201ff270faacd7e4e4a67a12410e26410f2be
ygrek <ygrek@autistici.org>
2009-10-17 12:19:56 +0300

    examples/omulti: accept command-line args

--- examples/omulti.ml
+++ examples/omulti.ml
@@ -18,10 +18,29 @@ let loop mt =
   let n = rm 0 in
   Printf.printf "Removed %u handles\n" n
 
+let input_lines file =
+  let ch = open_in file in
+  let lines = ref [] in
+  try while true do lines := input_line ch :: !lines done; []
+  with End_of_file -> close_in_noerr ch; List.rev !lines
+
 let () =
-  let n = try int_of_string (Sys.argv.(1)) with _ -> 10 in
-  let module C = Array in
-  let urls = C.make n "http://localhost:8080/files/test.tar.gz" in
+  let module A = Array in
+  let urls = 
+    let urls = ref [] in
+    let n = ref 10 in
+    let args = Arg.align 
+      ["-n",Arg.Set_int n,"<N> ";
+       "-i",Arg.String (fun s -> urls := input_lines s @ !urls),"<file> read urls from file";
+       "-l",Arg.String (fun s -> urls := s :: !urls),"<url> fetch url";
+      ]
+    in
+    Arg.parse args failwith "Options :";
+    match !urls with
+    | [url] -> A.make !n url
+    | l -> A.of_list l
+  in
+(*   if A.length urls = 0 then failwith "Specify urls to download"; *)
   let init url =
     let h = Curl.init () in
     Curl.set_url h url;
@@ -36,11 +55,12 @@ let () =
       (Curl.get_effectiveurl h);
     Curl.cleanup h
   in
-  let hs = C.map init urls in
+  let hs = A.map init urls in
   let mt = M.create () in
-  C.iter (M.add mt) hs;
+  A.iter (M.add mt) hs;
   loop mt;
-  C.iter cleanup hs;
+  A.iter cleanup hs;
   M.cleanup mt;
+  print_endline "Finished";
   ()
 
