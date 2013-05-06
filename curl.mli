c92f1dc2689902fe8ba9bc3f9424c7f7c86d9aa6
ygrek <ygrek@autistici.org>
2013-05-06 18:31:11 +0800

    make SSLVERIFYHOST_EXISTENCE equivalent to SSLVERIFYHOST_HOSTNAME
    Previous workaround was broken, so better keep it simple.

--- curl.mli
+++ curl.mli
@@ -146,9 +146,7 @@ type curlClosePolicy =
 
 type curlSSLVerifyHost =
   | SSLVERIFYHOST_NONE (** connection succeeds regardless of the names in the certificate *)
-  | SSLVERIFYHOST_EXISTENCE (** certificate must contain a Common Name field, but it doesn’t matter what name it says
-                                (libcurl >= 7.28.0 will error out on this option, in such cases this library will pass
-                                SSLVERIFYHOST_HOSTNAME to libcurl instead) *)
+  | SSLVERIFYHOST_EXISTENCE (** deprecated, equivalent to SSLVERIFYHOST_HOSTNAME *)
   | SSLVERIFYHOST_HOSTNAME (** certificate must indicate the matching hostname, or the connection fails *)
 
 type curlHTTPVersion =
--- examples/ossl.ml
+++ examples/ossl.ml
@@ -4,15 +4,15 @@
  * Copyright (c) 2003-2008, Lars Nilsson, <lars@quantumchamaeleon.com>
  *)
 
-let _ =
-  if (Array.length Sys.argv) = 2 then
+let () =
+  if Array.length Sys.argv = 2 then
     begin
       Curl.global_init Curl.CURLINIT_GLOBALALL;
       let connection = Curl.init () in
-	Curl.set_url connection Sys.argv.(1);
-	Curl.set_sslverifypeer connection false;
-	Curl.set_sslverifyhost connection Curl.SSLVERIFYHOST_EXISTENCE;
-	Curl.perform connection
+      Curl.set_url connection Sys.argv.(1);
+      Curl.set_sslverifypeer connection true;
+      Curl.set_sslverifyhost connection Curl.SSLVERIFYHOST_HOSTNAME;
+      Curl.perform connection
     end
   else
     Printf.fprintf stderr "Usage: %s <url>\n" Sys.argv.(0)
