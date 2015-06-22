b537a2b7259d2230b66d6aa3e104cb0e54beddc5
ygrek <ygrek@autistici.org>
2015-06-21 17:46:14 -0700

    Makefile: detect ext_dll

---
+++ print_ext.ml
@@ -0,0 +1,7 @@
+#use "topfind";;
+#require "compiler-libs.common";;
+match List.tl (Array.to_list Sys.argv) with
+| ["dll"] -> print_endline Config.ext_dll
+| ["obj"] -> print_endline Config.ext_obj
+| ["lib"] -> print_endline Config.ext_lib
+| _ -> prerr_endline "print_ext.ml: wrong usage"; exit 2
